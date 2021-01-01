d0b3d5c76aee191dadcdbc3ddbc4b4a48f780c24
s_erfani <saeederfani75@gmail.com>
2021-01-01 20:42:58 +0330

    fix bug for negative numbers

--- lib/calculator_screen.dart
+++ lib/calculator_screen.dart
@@ -42,16 +42,16 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
             a = (double.parse(a) + double.parse(b)).toString();
             result = a.endsWith(".0") ? a.substring(0, a.length - 2) : a;
             b = "";
-          } else if (result.contains("-")) {
-            a = (double.parse(a) - double.parse(b)).toString();
-            result = a.endsWith(".0") ? a.substring(0, a.length - 2) : a;
-            b = "";
           } else if (result.contains("*")) {
             a = (double.parse(a) * double.parse(b)).toString();
             result = a.endsWith(".0") ? a.substring(0, a.length - 2) : a;
             b = "";
           } else if (result.contains("/")) {
             a = (double.parse(a) / double.parse(b)).toString();
+            result = a.endsWith(".0") ? a.substring(0, a.length - 2) : a;
+            b = "";
+          } else if (result.contains("-")) {
+            a = (double.parse(a) - double.parse(b)).toString();
             result = a.endsWith(".0") ? a.substring(0, a.length - 2) : a;
             b = "";
           }
